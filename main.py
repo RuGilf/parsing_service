b44fbc9c26eec9caabbc16b5c699cc16d798b0be
RuGilf <hackjackbrain@gmail.com>
2025-08-05 15:10:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,32 +6,31 @@ headers = {'user-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko
             'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
 }
 
-url = 'https://misis.ru/applicants/admission/progress/baccalaureate-and-specialties/spiskipodavshihzayavleniya/list-p/?id=BVO-COMM-O-090000-NITU_MISIS-OKM-000005382'
+def update_script(url):
+    response = requests.get(url, headers=headers)
 
-response = requests.get(url, headers=headers)
-
-with open('data.csv', mode='w', newline='') as file:
-    fieldnames = ['id', 'уникальный номер', 'приоритет зачисления', 'сумма баллов', 'согласие на зачисление']
-    csv_writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')  # <- delimiter=';'
-    csv_writer.writeheader()
+    with open('data.csv', mode='w', newline='') as file:
+        fieldnames = ['id', 'уникальный номер', 'приоритет зачисления', 'сумма баллов', 'согласие на зачисление']
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')  # <- delimiter=';'
+        csv_writer.writeheader()
     
-    soup = BeautifulSoup(response.text, "html.parser")
-    rows = soup.find('table', class_='data with-hover sortable js-fixed js-table-data').find('tbody').find_all('tr')
+        soup = BeautifulSoup(response.text, "html.parser")
+        rows = soup.find('table', class_='data with-hover sortable js-fixed js-table-data').find('tbody').find_all('tr')
     
-    for row in rows: 
-        columns = row.find_all('td') 
+        for row in rows: 
+            columns = row.find_all('td') 
 		
-        if columns[17].text == '' or None:
-            agree = '-'
-        else:
-            agree = '+'
+            if columns[17].text == '' or None:
+                agree = '-'
+            else:
+                agree = '+'
 
-        data = {
-            'id': columns[0].text.strip(), 
-            'уникальный номер': columns[1].text.strip(), 
-            'приоритет зачисления': columns[3].text.strip(), 
-            'сумма баллов': columns[4].text.strip(), 
-            'согласие на зачисление': agree
-        }
+            data = {
+                'id': columns[0].text.strip(), 
+                'уникальный номер': columns[1].text.strip(), 
+                'приоритет зачисления': columns[3].text.strip(), 
+                'сумма баллов': columns[4].text.strip(), 
+                'согласие на зачисление': agree
+            }
         
-        csv_writer.writerow(data)
+            csv_writer.writerow(data)
